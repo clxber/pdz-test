f23b250b2716a7846f9e9081f738fd2d5b0a798b
clxber <89441564+clxber@users.noreply.github.com>
2026-08-16 01:08:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 import sys
 
 try:
-    r = requests.head('http://bfts.5read.com/pdz/15431837unRegister.pdz', timeout=10)
+    r = requests.head('http://bfts.5read.com/pdz/12037216unRegister.pdz', timeout=10)
     print('Status Code:', r.status_code)
     print('Headers:', dict(r.headers))
     sys.exit(0)
